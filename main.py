9910482a0910d2120d7405596c21279336fb0e0e
armmyda123 <s7070128@kmitl.ac.th>
2017-11-10 14:54:15 +0700

    update testing graph

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+import plotly
+import plotly.plotly as py
+from plotly.graph_objs import *
+def main():
+    lis_x = [2547, 2548, 2549, 2550, 2547, 2548, 2549, 2550, 2547, 2548, 2549, 2550]
+    plotly.offline.plot(({
+        "data": [Scatter(x=lis_x, y=[10, 20])],
+        "layout": Layout(title="Sample Test")
+    }))
+main()
