6dabb1badfd3b0b70cb5d51e67c5ffbfc14577f2
armmyda123 <s7070128@kmitl.ac.th>
2017-12-12 22:16:34 +0700

    Update chk.py
    Fixed Some Code

--- chk.py
+++ chk.py
@@ -1,7 +1,8 @@
 import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly
-
+""" This code makeing Pie Graph respresent all data of Popular Song and \
+Non-Popular Song """
 labels = ['Non-Popular Song', 'Popular Song']
 values = [232, 232]
 trace = go.Pie(labels=labels, values=values)
