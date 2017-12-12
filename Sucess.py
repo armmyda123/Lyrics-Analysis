7104650f9d6842924427a740f5e7979e9aa144d5
makezadsc <makeapisit@HOTMAIL.COM>
2017-12-12 20:26:35 +0700

    Add files via upload

---
+++ Sucess.py
@@ -0,0 +1,12 @@
+import plotly
+import plotly.plotly as py
+from plotly.graph_objs import *
+def main():
+    lis_x = [108, 107, 98, 73, 69, 49, 46, 45, 42, 39, 34, 27, 25, 24, 20, 18, 11, 11, 6, 6]
+    lis_y = ['Time', 'Baby', 'Take', 'Keep', 'Good', 'Life', 'Little', 'Mind', 'Get', 'Eye', 'Fall',\
+             'Fire', 'Dream', 'Dance', 'Rock', 'Hell', 'Music', 'Lone', 'Thang', 'Die']
+    plotly.offline.plot(({
+        "data": [Pie(labels=lis_y, values=lis_x)],
+        "layout": Layout(title="Popular Song")
+    }))
+main()
